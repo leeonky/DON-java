0e88bad6c3e24c53a372931a51467b23cec28230
Liangchen <leeonky@gmail.com>
2021-12-30 22:55:38 +0800

    Refactor

--- src/main/java/com/github/leeonky/jsontable/Numbers.java
+++ src/main/java/com/github/leeonky/jsontable/Numbers.java
@@ -1,24 +1,53 @@
 package com.github.leeonky.jsontable;
 
 public class Numbers {
+
     public static Number parseNumber(String content) {
+        return new Numbers().parse(content);
+    }
+
+    private Number parse(String content) {
         if (content.length() == 0)
             return null;
-        int negative = 1;
-        int charIndex = 0;
-        if (content.charAt(0) == '-') {
-            negative = -1;
-            charIndex++;
-        } else if (content.charAt(0) == '+') {
-            charIndex++;
-        }
-        int value = 0;
-        while (charIndex < content.length()) {
-            int digit = Character.digit(content.charAt(charIndex++), 10);
-            if (digit < 0)
-                return null;
-            value = value * 10 + digit;
-        }
+        Token token = new Token(content);
+        int negative = token.getSign();
+        Integer value = token.getInteger();
+        if (value == null)
+            return null;
         return negative * value;
+    }
+
+    private static class Token {
+        int index;
+        final char[] chars;
+
+        private Token(String code) {
+            chars = code.toCharArray();
+        }
+
+        private boolean takeChar(char c) {
+            boolean result;
+            if (result = chars[index] == c)
+                index++;
+            return result;
+        }
+
+        public Integer getInteger() {
+            int value = 0;
+            while (index < chars.length) {
+                int digit = Character.digit(chars[index++], 10);
+                if (digit < 0)
+                    return null;
+                value = value * 10 + digit;
+            }
+            return value;
+        }
+
+        public int getSign() {
+            if (takeChar('-'))
+                return -1;
+            takeChar('+');
+            return 1;
+        }
     }
 }
