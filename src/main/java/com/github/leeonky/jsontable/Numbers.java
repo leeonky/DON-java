947e26023c916b8ff5696ffb9a2b5f0b8e34232b
Liangchen <leeonky@gmail.com>
2022-01-01 16:59:10 +0800

    HEX integer

--- src/main/java/com/github/leeonky/jsontable/Numbers.java
+++ src/main/java/com/github/leeonky/jsontable/Numbers.java
@@ -12,12 +12,14 @@ public class Numbers {
         if (content.length() == 0)
             return null;
         Token token = new Token(content);
-        return token.parseInteger(token.getSign());
+        int sign = token.getSign();
+        int radix = token.getRadix();
+        return token.parseInteger(sign, radix);
     }
 
     private static class Token {
-        int index;
-        final char[] chars;
+        private int index;
+        private final char[] chars;
 
         private Token(String code) {
             chars = code.toCharArray();
@@ -30,50 +32,72 @@ public class Numbers {
             return result;
         }
 
+        private boolean startWith(String str) {
+            if (index + str.length() < chars.length) {
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) != chars[index + i])
+                        return false;
+                }
+                index += str.length();
+                return true;
+            }
+            return false;
+        }
+
+        public int getRadix() {
+            if (startWith("0x") || startWith("0X"))
+                return 16;
+            return 10;
+        }
+
         public static class IntegerBoundary {
-            final int limit;
-            final int limitBeforeMul;
+            private final int limit;
+            private final int limitBeforeMul;
+            private final int radix;
 
             public IntegerBoundary(int negative, int radix) {
                 limit = negative > 0 ? -Integer.MAX_VALUE : Integer.MIN_VALUE;
                 limitBeforeMul = limit / radix;
+                this.radix = radix;
             }
 
             private boolean isOverflow(int value, int digit) {
-                return value < limitBeforeMul || value * 10 < limit + digit;
+                return value < limitBeforeMul || value * radix < limit + digit;
             }
         }
 
-        public Number parseInteger(int negative) {
+        public Number parseInteger(int negative, int radix) {
             if (isTheEnd())
                 return null;
             int value = 0;
-            IntegerBoundary integerBoundary = new IntegerBoundary(negative, 10);
+            IntegerBoundary integerBoundary = new IntegerBoundary(negative, radix);
             for (char c : leftChars()) {
                 if (c == '_') {
                     if (isTheEnd())
                         return null;
                     continue;
                 }
-                int digit = Character.digit(c, 10);
+                int digit = Character.digit(c, radix);
                 if (digit < 0)
                     return null;
                 if (integerBoundary.isOverflow(value, digit))
-                    return parseLong(negative, ((long) value * 10) - digit);
-                value = value * 10 - digit;
+                    return parseLong(((long) value * radix) - digit, negative, radix);
+                value = value * radix - digit;
             }
             return -negative * value;
         }
 
-        private Number parseLong(int negative, long value) {
+        private Number parseLong(long value, int negative, int radix) {
             for (char c : leftChars()) {
                 if (c == '_') {
                     if (isTheEnd())
                         return null;
                     continue;
                 }
-                int digit = Character.digit(c, 10);
-                value = value * 10 - digit;
+                int digit = Character.digit(c, radix);
+                if (digit < 0)
+                    return null;
+                value = value * radix - digit;
             }
             return -negative * value;
         }
--- src/test/java/com/github/leeonky/jsontable/NumbersTest.java
+++ src/test/java/com/github/leeonky/jsontable/NumbersTest.java
@@ -11,50 +11,126 @@ class NumbersTest {
     @Nested
     class IntegerNumber {
 
-        @Test
-        void parse_int_number() {
-            assertParse("0", 0);
-            assertParse("1", 1);
-            assertParse("24", 24);
-            assertParse("1_000", 1_000);
-            assertParse("+24", 24);
-            assertParse("2147483647", 2147483647);
+        @Nested
+        class Radix10 {
+
+            @Test
+            void parse_int_number() {
+                assertParse("0", 0);
+                assertParse("1", 1);
+                assertParse("24", 24);
+                assertParse("1_000", 1_000);
+                assertParse("+24", 24);
+                assertParse("2147483647", 2147483647);
+            }
+
+            @Test
+            void negative() {
+                assertParse("-24", -24);
+                assertParse("-2147483648", -2147483648);
+            }
+
+            @Test
+            void int_over_flow() {
+                assertParse("2147483648", 2147483648L);
+                assertParse("2147483657", 2147483657L);
+                assertParse("-2147483649", -2147483649L);
+                assertParse("-2147483658", -2147483658L);
+            }
+
+            @Test
+            void invalid_number() {
+                assertParse("+", null);
+                assertParse("-", null);
+                assertParse("1_", null);
+                assertParse("1x", null);
+            }
         }
 
-        @Test
-        void parse_negative_int_number() {
-            assertParse("-24", -24);
-            assertParse("-2147483648", -2147483648);
-        }
+        @Nested
+        class Radix16 {
 
-        @Test
-        void int_over_flow() {
-            assertParse("2147483657", 2147483657L);
-            assertParse("2147483648", 2147483648L);
-            assertParse("-2147483649", -2147483649L);
-            assertParse("-2147483658", -2147483658L);
-        }
+            @Test
+            void parse_int_number() {
+                assertParse("0x0", 0);
+                assertParse("0xf", 0xf);
+                assertParse("0XF", 0XF);
+                assertParse("0x1_000", 0x1_000);
+                assertParse("+0xff", 0xff);
+                assertParse("0x7fffffff", 2147483647);
+            }
 
-        @Test
-        void invalid_number() {
-            assertParse("+", null);
-            assertParse("-", null);
-            assertParse("1_", null);
+            @Test
+            void negative() {
+                assertParse("-0x1f", -0x1f);
+                assertParse("-0x80000000", -0x80000000);
+            }
+
+            @Test
+            void int_over_flow() {
+                assertParse("0x80000000", 0x80000000L);
+                assertParse("-0x80000001", -0x80000001L);
+            }
+
+            @Test
+            void invalid_number() {
+                assertParse("+0x", null);
+                assertParse("-0x", null);
+                assertParse("0x1_", null);
+                assertParse("0x1x", null);
+            }
         }
     }
 
     @Nested
     class ParseLong {
 
-        @Test
-        void parse_long() {
-            assertParse("100000000005", 100000000005L);
-            assertParse("100000000005_000", 100000000005_000L);
+        @Nested
+        class Radix10 {
+
+            @Test
+            void parse_long() {
+                assertParse("100000000005", 100000000005L);
+                assertParse("100000000005_000", 100000000005_000L);
+                assertParse("9223372036854775807", 9223372036854775807L);
+            }
+
+            @Test
+            void negative() {
+                assertParse("0x80000010", 0x80000010L);
+                assertParse("-0x80000010", -0x80000010L);
+                assertParse("-9223372036854775808", -9223372036854775808L);
+            }
+
+            @Test
+            void invalid_number() {
+                assertParse("100000000005_", null);
+                assertParse("100000000005xx", null);
+            }
         }
 
-        @Test
-        void invalid_number() {
-            assertParse("100000000005_", null);
+        @Nested
+        class Radix16 {
+
+            @Test
+            void parse_long() {
+                assertParse("0xfffffffffff", 0xfffffffffffL);
+                assertParse("0xfff_ffff_ffff", 0xfff_ffff_ffffL);
+                assertParse("0x7fffffffffffffff", 9223372036854775807L);
+            }
+
+            @Test
+            void negative() {
+                assertParse("0x80000010", 0x80000010L);
+                assertParse("-0x80000010", -0x80000010L);
+                assertParse("-0x8000000000000000", -9223372036854775808L);
+            }
+
+            @Test
+            void invalid_number() {
+                assertParse("100000000005_", null);
+                assertParse("100000000005xx", null);
+            }
         }
     }
 
